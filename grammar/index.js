2e426c4f092cbfa66549f7ad9ad5d14e2de73e1a
Sumingcheng <fanxiaohaohaha@qq..com>
2022-12-29 15:05:55 +0800

    修改案例

---
+++ grammar/index.js
@@ -0,0 +1,17 @@
+//  jQuery 的入口函数是在 html 所有标签(DOM)都加载之后，就会去执行。
+//  JavaScript 的 window.onload 事件是等到所有内容，包括外部图片之类的文件加载完后，才会执行。
+
+$(function () {
+  // 隐藏/显示 display toggle=hide+show
+  $('#b1').click(function () {
+    // $('.d1').hide(1000);
+    $('.d1').toggle(1000);
+  });
+
+  // 淡入淡出
+  $('#b2').click(function () {
+    // $('.d2').fadeIn();
+    // $('.d2').fadeOut();
+    $('.d2').fadeToggle(1000);
+  });
+});
