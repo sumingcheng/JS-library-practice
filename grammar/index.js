28e54e473d679f3201ce9a7ba5707113e07d8555
Sumingcheng <fanxiaohaohaha@qq..com>
2022-12-31 15:04:12 +0800

    2022年12月31日 15:04:06

--- grammar/index.js
+++ grammar/index.js
@@ -1,6 +1,6 @@
 //  jQuery 的入口函数是在 html 所有标签(DOM)都加载之后，就会去执行。
 //  JavaScript 的 window.onload 事件是等到所有内容，包括外部图片之类的文件加载完后，才会执行。
-
+let p = `<p>追加元素</p>`;
 $(function () {
   // 隐藏/显示 display toggle=hide+show
   $('#b1').on('click', function () {
@@ -32,7 +32,7 @@ $(function () {
       $(this).css('backgroundColor', '#000');
     });
   });
-  
+
   // text html 可修改元素内文字
   // val 可修改表单内数据
   $('#b5').on('click', function () {
@@ -50,4 +50,40 @@ $(function () {
       console.log(index, oldvalue);
     });
   });
+
+  // 添加元素
+  $('#b7').on('click', function () {
+    // 追加内容
+    $('.d7').append(p);
+    // 开头插入
+    // $('.d7').prepend(p);
+  });
+
+  // 被选元素之后插入内容
+  $('#b8').on('click', function () {
+    // $('.d8').after('11111111');
+    $('.d8').before(`
+      <div>div</div>
+    `);
+  });
+
+  // 删除元素
+  $('#b9').on('click', function () {
+    // 删除内容
+    $('.d9').empty();
+    // 删除元素
+    // $('.d9').remove();
+  });
+
+  // 添加/删除类
+  $('#b10').on('click', function () {
+    // 添加类
+    // $('.d10').addClass('NiuB2');
+    // 删除类
+    // $('.d10').removeClass('NiuB');
+    // 添加删除
+    $('.d10').toggleClass('NiuB3');
+  });
+
+
 });
